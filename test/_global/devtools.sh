f0c27778a6486850ecf69c37d8d3f392a7b6aeaa
Kevin McBride <681929+krmcbride@users.noreply.github.com>
2022-11-21 13:59:27 -0800

    zsh: refactor on assumption of common-utils, git

--- test/_global/devtools.sh
+++ test/_global/devtools.sh
@@ -4,7 +4,10 @@ set -e
 source dev-container-features-test-lib
 
 check "zsh is installed" zsh --version
-check "vi is installed" vim --version
-check "vim is installed" vim --version
+check "oh-my-zsh is installed" test -d "/root/.oh-my-zsh/"
+check "omz custom aliases" grep 'alias gcaa' /root/.oh-my-zsh/custom/aliases.zsh
+check ".zshrc is installed" test -f "/root/.zshrc"
+# check "pure-prompt is installed" test -d "/usr/local/pure-prompt/"
+# check ".zshrc pure path" grep 'fpath+=(/usr/local/pure-prompt)' /root/.zshrc
 
 reportResults
--- test/zsh/test.sh
+++ test/zsh/test.sh
@@ -4,11 +4,11 @@ set -e
 source dev-container-features-test-lib
 
 check "zsh is installed" zsh --version
-check "oh-my-zsh is installed" test -d "/usr/local/oh-my-zsh/"
-check "omz custom aliases" grep 'alias gcaa' /usr/local/oh-my-zsh/custom/aliases.zsh
-check "pure-prompt is installed" test -d "/usr/local/pure-prompt/"
-check ".zshrc is installed" test -f "/root/.zshrc"
-check ".zshrc omz path" grep 'source /usr/local/oh-my-zsh/oh-my-zsh.sh' /root/.zshrc
-check ".zshrc pure path" grep 'fpath+=(/usr/local/pure-prompt)' /root/.zshrc
+# check "oh-my-zsh is installed" test -d "/usr/local/oh-my-zsh/"
+# check "omz custom aliases" grep 'alias gcaa' /usr/local/oh-my-zsh/custom/aliases.zsh
+# check "pure-prompt is installed" test -d "/usr/local/pure-prompt/"
+# check ".zshrc is installed" test -f "/root/.zshrc"
+# check ".zshrc omz path" grep 'source /usr/local/oh-my-zsh/oh-my-zsh.sh' /root/.zshrc
+# check ".zshrc pure path" grep 'fpath+=(/usr/local/pure-prompt)' /root/.zshrc
 
 reportResults
