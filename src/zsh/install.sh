e2456f4570cdbd4a93e4a9a8754686c262df5040
Kevin McBride <681929+krmcbride@users.noreply.github.com>
2022-11-21 12:21:23 -0800

    zsh: add aliases

--- src/zsh/install.sh
+++ src/zsh/install.sh
@@ -47,3 +47,30 @@ fpath+=(${PURE_PATH})
 autoload -U promptinit; promptinit
 prompt pure
 EOF
+
+cat > ${OMZ_PATH}/custom/aliases.zsh \
+<< EOF
+# shell
+alias c='clear'
+alias l='ls -hl'
+alias all='l -A'
+alias ll='l -A'
+# Git
+alias g='git'
+alias ga='git add'
+alias gall='git add -A'
+alias gd='git diff'
+alias gl='git log -10'
+alias gs='git status'
+alias gss='git status -s'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gcm='git commit -v -m'
+alias gg="git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative"
+alias ggs="gg --stat"
+# From http://blogs.atlassian.com/2014/10/advanced-git-aliases/
+# Show commits since last pull
+alias gnew="git log HEAD@{1}..HEAD@{0}"
+# Add uncommitted and unstaged changes to the last commit
+alias gcaa="git commit -a --amend -C HEAD"
+EOF
--- test/zsh/test.sh
+++ test/zsh/test.sh
@@ -5,6 +5,7 @@ source dev-container-features-test-lib
 
 check "zsh is installed" zsh --version
 check "oh-my-zsh is installed" test -d "/usr/local/oh-my-zsh/"
+check "omz custom aliases" grep 'alias gcaa' /usr/local/oh-my-zsh/custom/aliases.zsh
 check "pure-prompt is installed" test -d "/usr/local/pure-prompt/"
 check ".zshrc is installed" test -f "/root/.zshrc"
 check ".zshrc omz path" grep 'source /usr/local/oh-my-zsh/oh-my-zsh.sh' /root/.zshrc
